ead0cc186737b2610e9813b37b13d53cd022d6a2
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2025-02-12 19:30:21 +0800

    Create task model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    class Status(models.IntegerChoices):
+        BACKLOG = 0, "Backlog"
+        DOING = 1, "Doing"
+        DONE = 2, "Done"
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    status = models.IntegerField(choices=Status.choices, default=Status.BACKLOG)
+    due_date = models.DateField(null=True, blank=True)
+    assignee = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.assignee})"
