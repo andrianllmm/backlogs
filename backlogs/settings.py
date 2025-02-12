3d65d01db4534a4423dcc49d48dc226c0c2aa85d
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2025-02-12 19:05:28 +0800

    Create custom user model

--- backlogs/settings.py
+++ backlogs/settings.py
@@ -131,3 +131,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+
+AUTH_USER_MODEL = "users.User"
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    add_form = CustomUserCreationForm
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            "Other",
+            {"fields": ("phone_number",)},
+        ),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
