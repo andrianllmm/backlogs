25098fe2488a17f85c2ea8969907df51e98568e3
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2025-06-10 22:49:16 +0800

    Fix register issue with username

--- users/forms.py
+++ users/forms.py
@@ -41,6 +41,7 @@ class RegisterForm(UserCreationForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
+        user.username = self.cleaned_data["email"]
         user.email = self.cleaned_data["email"]
         user.set_password(self.cleaned_data["password1"])
 
--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
